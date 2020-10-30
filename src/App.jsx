8255c014a0825c5df6de0648b879796cceeba821
Samuel Trahan <samtrahan91@gmail.com>
2020-10-29 20:51:32 -0500

    genre drop down

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import NavBar from './components/NavBar';
+import DropDown from './components/DropDown/DropDown';
 import './App.css';
 
 export default function App() {
   return (
     <>
       <NavBar />
+      <h1 className="title">NotSpotify</h1>
+      <DropDown />
     </>
   );
 }
---
+++ src/components/DropDown/DropDown.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './DropDown.css';
+
+export default function DropDown() {
+  return (
+    <div className="drop-down">
+      <div className="drop-down-item">
+        <select className="select ui dropdown">
+          <option disabled value="" placeholder="Genre">
+            Genre
+          </option>
+          <option value="1">Hip-Hop</option>
+          <option value="2">Rap</option>
+          <option value="3">R&B</option>
+          <option value="4">Jazz</option>
+          <option value="5">Rock</option>
+          <option value="6">Electronic Dance Music</option>
+          <option value="7">Soul Music</option>
+          <option value="8">Country</option>
+          <option value="9">Pop</option>
+          <option value="10">LO-FI Beats</option>
+        </select>
+      </div>
+    </div>
+  );
+}
